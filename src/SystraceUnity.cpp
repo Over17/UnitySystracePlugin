f9c41700e09ac9b5a2502181d577ece81505936d
Yury Habets <over17@outlook.com>
2018-06-28 13:39:42 +0200

    Switched to dynamic linking to the trace API to support Android before Marshmallow

--- src/SystraceUnity.cpp
+++ src/SystraceUnity.cpp
@@ -1,12 +1,23 @@
-#include <android/trace.h>
+#include <dlfcn.h>
+
 #include "IUnityInterface.h"
 #include "IUnityProfilerCallbacks.h"
 
 static IUnityProfilerCallbacks* s_UnityProfilerCallbacks = NULL;
 
+typedef void (*fp_ATrace_beginSection) (const char* sectionName);
+typedef void (*fp_ATrace_endSection) ();
+typedef bool (*fp_ATrace_isEnabled) ();
+
+static fp_ATrace_beginSection ATrace_beginSection;
+static fp_ATrace_endSection ATrace_endSection;
+static fp_ATrace_isEnabled ATrace_isEnabled;
+
+static void* libandroid;
+
 static void UNITY_INTERFACE_API SystraceEventCallback(const UnityProfilerMarkerDesc* eventDesc, UnityProfilerMarkerEventType eventType, unsigned short eventDataCount, const UnityProfilerMarkerData* eventData, void* userData)
 {
-    if (!ATrace_isEnabled())
+    if (ATrace_isEnabled && !ATrace_isEnabled())
         return;
 
     switch (eventType)
@@ -31,12 +42,25 @@ static void UNITY_INTERFACE_API SystraceCreateEventCallback(const UnityProfilerM
 
 extern "C" void UNITY_INTERFACE_EXPORT UNITY_INTERFACE_API UnityPluginLoad(IUnityInterfaces* unityInterfaces)
 {
-    s_UnityProfilerCallbacks = unityInterfaces->Get<IUnityProfilerCallbacks>();
-    s_UnityProfilerCallbacks->RegisterCreateMarkerCallback(SystraceCreateEventCallback, NULL);
+    // Doing dynamic linking because native trace API is supported only in Android M+
+    libandroid = dlopen("libandroid.so", RTLD_NOW | RTLD_LOCAL);
+    if (libandroid != NULL)
+    {
+        ATrace_isEnabled = reinterpret_cast<fp_ATrace_isEnabled >(dlsym(libandroid, "ATrace_isEnabled"));
+        ATrace_beginSection = reinterpret_cast<fp_ATrace_beginSection >(dlsym(libandroid, "ATrace_beginSection"));
+        ATrace_endSection = reinterpret_cast<fp_ATrace_endSection >(dlsym(libandroid, "ATrace_endSection"));
+
+        s_UnityProfilerCallbacks = unityInterfaces->Get<IUnityProfilerCallbacks>();
+        s_UnityProfilerCallbacks->RegisterCreateMarkerCallback(SystraceCreateEventCallback, NULL);
+    }
 }
 
 extern "C" void UNITY_INTERFACE_EXPORT UNITY_INTERFACE_API UnityPluginUnload()
 {
-    s_UnityProfilerCallbacks->UnregisterCreateMarkerCallback(SystraceCreateEventCallback, NULL);
-    s_UnityProfilerCallbacks->UnregisterMarkerEventCallback(NULL, SystraceEventCallback, NULL);
+    if (libandroid != NULL)
+    {
+        s_UnityProfilerCallbacks->UnregisterCreateMarkerCallback(SystraceCreateEventCallback, NULL);
+        s_UnityProfilerCallbacks->UnregisterMarkerEventCallback(NULL, SystraceEventCallback, NULL);
+        dlclose(libandroid);
+    }
 }
