a462aeb1631baf9383dff9015b9715f486ebde2a
Yury Habets <over17@outlook.com>
2018-06-28 15:28:24 +0200

    Enable more warnings

--- jni/Android.mk
+++ jni/Android.mk
@@ -1,10 +1,9 @@
 include $(CLEAR_VARS)
 LOCAL_PATH      := $(NDK_PROJECT_PATH)
 LOCAL_MODULE    := systrace_unity
-LOCAL_CFLAGS    := -Werror
+LOCAL_CFLAGS    := -Wall -Werror
 
 LOCAL_SRC_FILES := src/SystraceUnity.cpp
-LOCAL_LDLIBS    += -landroid
-LOCAL_LDLIBS    += -llog
+LOCAL_LDLIBS    += -landroid -llog
 
 include $(BUILD_SHARED_LIBRARY)
