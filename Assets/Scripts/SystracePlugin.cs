cbcae8beddc8a32d04532f5e300ed353fd6f432d
Yury Habets <over17@outlook.com>
2018-06-29 10:54:04 +0200

    Added Unity project

---
+++ Assets/Scripts/SystracePlugin.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Runtime.InteropServices;
+
+public class SystracePlugin : MonoBehaviour
+{
+#if UNITY_ANDROID && !UNITY_EDITOR
+    [DllImport("systrace_unity")]
+    private static extern void InitSystraceUnityPlugin();
+
+    void Start ()
+    {
+        InitSystraceUnityPlugin();
+    }
+#endif
+}
